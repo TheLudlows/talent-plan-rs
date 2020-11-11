e4b210eeed06b7c3133d099da1d26866f9133b57
four <ai-l@outlook.com>
2020-11-11 19:11:30 +0800

    project 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # talent-plan-rs
+[Project-1](https://github.com/TheLudlows/talent-plan-rs/tree/project-1)
+[Project-2](https://github.com/TheLudlows/talent-plan-rs/tree/project-2)
--- src/bin/client_start.rs
+++
@@ -1,8 +0,0 @@
-use rs_kv::client::DBClient;
-
-fn main() {
-    let mut client = DBClient::connect("localhost:8888").unwrap();
-    client.set("1".to_string(),"2".to_string());
-    let res = client.get("1".to_string());
-    println!("{}",res.unwrap())
-}
